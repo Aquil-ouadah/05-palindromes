8462997dedf37b38dc5cfb22c8f4c559a20734e1
Aquil-ouadah <aquil.ouadah@edu.esiee.fr>
2024-11-14 10:32:42 +0000

    Definition of functions

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 #### Fonction secondaire
+"""Ce code permet de vérifier si une chaîne de caractère est un palindrome"""
 
+def ispalindrome(s):
+    """Fonction qui vérifie si une chaîne de caractère s est un palindrome"""
+    s = s.lower()
 
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    changement_de_var = str.maketrans('éèêëàâäîïôöùûüç', 'eeeeaaaiioouuuc', ' ",?;.:/!\'-')
+    s = s.translate(changement_de_var)
+    s_inv = s[::-1]
+    return s == s_inv
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """Cette fonction est la fonction principale"""
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
